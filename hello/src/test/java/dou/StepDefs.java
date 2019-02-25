62e9ee95651131c3435e815ee23bfb8a0f1c76d3
Juan Hernandez <juan.hernandez@digitalonus.com>
2019-02-25 10:21:58 -0600

    adding step defs

---
+++ hello/src/test/java/dou/StepDefs.java
@@ -0,0 +1,21 @@
+package dou;
+
+public class StepDefs{
+    @Given("^I have a hello app with \"([^\"]*)\"$")
+public void i_have_a_hello_app_with(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I ask it to say hi$")
+public void i_ask_it_to_say_hi() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^it should answer \"([^\"]*)\"$")
+public void it_should_answer(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+}
